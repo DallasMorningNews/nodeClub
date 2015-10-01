1dc428d1cb3b7034c1b45f1290e86493b2257195
Jon <jon.r.mcclure@gmail.com>
2015-10-01 09:40:52 -0500

    Jon's minor mods on Layne's comments

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 I.      IMPORTANT FILES/FOLDERS IN THE DIRECTORIES
 ---------------------------------------------------------
 FILES
-package.json - the shopping list of all our dependencies
-.env -- environment file where we can save private database credentials
+package.json - the shopping list of all our dependencies and meta info about the app
+.env -- environment file where we can save private database credentials as "environment variables"
 app.js -- main script the server runs
-nodemon.json -- give nodemon instructions, in this case script as well as static files
+nodemon.json -- nodemon config file. Basically tells it what folders to watch for changes and restart the app.
 
 FOLDERS
 node_modules -- Directory that holds actual dependencies listed in package.json
 static -- Directory that will hold all of our static files like images, CSS and client-side javascript
-models -- Directory where we'll hold our database/table code
+models -- Directory where we'll declare our "models", i.e., the structure of our database tables
 templates -- Directory for holding templates for site pages
 
 
@@ -24,7 +24,7 @@ var express = require('express'),
   nunjucks = require('nunjucks'),
   orm = require('orm');
 
-//In conjunction with dotenv dependency, allows access to data in .env file
+//In conjunction with dotenv dependency, allows access to environment variables in .env file
 require('dotenv').load();
 
 //Declare our app as an Express app
@@ -48,6 +48,7 @@ III.            NOW WE START DOING SOME WORK
 
 //------ DATABASE WORK
 //Our app is going to connect to our mysql database using credentials set in .env file
+//process.env object is created by the dotenv load line above
 app.use(orm.express("mysql://"+process.env.DB_USER+":"+process.env.DB_PASS+"@"+process.env.DB_HOST, {
     define: function (db, models) {
 
@@ -71,19 +72,22 @@ app.use(orm.express("mysql://"+process.env.DB_USER+":"+process.env.DB_PASS+"@"+p
 }));
 
 //------ SET ROUTE: A route takes info from the URL and turns it into variables used by the script
+// It also intercepts client requests, ie, URL requests, and serves up the appropriate page based on the rules here
+
 app.get('/home/:name', function (req, res) {     //When the URL has "/home/Layne" do the following...
     
-    //In the table called "person," find the var "Layne" in the column "name" and pass results back in "people" object...
+  //In the table called "person," find the var "Layne" in the column "name" and pass results back in array of "people" ...
   req.models.person.find({name:req.params.name}).run(function(err,people){    
     
-        //Render a page using the home template and pass the result of the "fullname()" method located in the models object above. 
+        //Render a page using the home.html template and pass the result of the "fullname()" method on the first 
+        //(and, in this case, only) object in the array we got back from find
         res.render("home.html",{'name' : people[0].fullname() });               
   });
 });
 
 
 /*-------------------------------------------------------
-IV.          BUILD SERVER TO SERVE PAGES
+IV.          BUILD SERVER TO SERVE PAGES LOCALLY
 -------------------------------------------------------*/
 
 var server = app.listen(3000, function () {
